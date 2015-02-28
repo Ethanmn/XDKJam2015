472cbc4255537e6a2f69ac4bf7c85276a020be14
aquira246 <aacost06@calpoly.edu>
2015-02-27 22:01:34 -0800

    improved soundplayer
    it now compiles in index :D

--- soundplayer.js
+++ soundplayer.js
@@ -2,3 +2,13 @@ var Hustle = new Audio("www/asset/sounds/Hustle.wav");
 var RoboWestern = new Audio("www/asset/sounds/Robo-Western.wav");
 var OutlawLand = new Audio("www/asset/sounds/584409_Outlaw-Land.wav");
 var laser = new Audio("www/asset/sounds/Gun_Shot-Marvin-1140816320.wav");
+
+var numSfx = 1;
+
+function playSFX(sfx) {
+	switch (sfx%numSfx) {
+		case 0:
+			laser.play();
+			break;
+	}
+}
