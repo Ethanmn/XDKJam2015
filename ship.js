372950f33600fc4310d856f993a1f7f069c01ee8
Simon Vurens <svurens@gmail.com>
2015-02-28 15:49:05 -0800

    stuff

--- ship.js
+++ ship.js
@@ -143,7 +143,7 @@ var Ship = function(tiles, cx, cy) {
       }
       ctx.fillStyle = '#ff0000';
       ctx.font = "16px Calibri";
-      ctx.fillText("Energy: " + Math.round(this.energy), 50, 50);
+      ctx.fillText("Energy: " + Math.floor(this.energy), 50, 50);
       ctx.stroke();
    }
    
