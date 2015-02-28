f7ebca373123ce064e87caac04c7c9d935c158c0
aquira246 <aacost06@calpoly.edu>
2015-02-27 22:13:36 -0800

    improved soundplayer.js
    added new functions to soundplayer.js
    use switchBackground to switch between the background music. this is
    untested at the moment

--- soundplayer.js
+++ soundplayer.js
@@ -3,12 +3,23 @@ var RoboWestern = new Audio("www/asset/sounds/Robo-Western.wav");
 var OutlawLand = new Audio("www/asset/sounds/584409_Outlaw-Land.wav");
 var laser = new Audio("www/asset/sounds/Gun_Shot-Marvin-1140816320.wav");
 
+var bgArray = [Hustle, RoboWestern, OutlawLand];
+var sfxArray = [laser];
+
 var numSfx = 1;
+var curBG = 0;
+var numBG = 3;
+
+function switchBackground (bg) {
+	bgArray[curBG].pause();
+	bgArray[curBG].currentTime = 0;
+
+	bgArray[bg%numBG].play();
+	curBG = bg%numBG;
+
+	return curBG;
+}
 
 function playSFX(sfx) {
-	switch (sfx%numSfx) {
-		case 0:
-			laser.play();
-			break;
-	}
+	sfxArray[sfx%numSfx];
 }
