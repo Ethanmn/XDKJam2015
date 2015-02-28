c807d7073152a5c032e59bedfaf00626ee45a1c6
aquira246 <aacost06@calpoly.edu>
2015-02-28 09:57:44 -0800

    merged assets folder
    everything is in the assets folder and the www folder is DEAD!

--- soundplayer.js
+++ soundplayer.js
@@ -1,7 +1,7 @@
-var Hustle = new Audio("www/asset/sounds/Hustle.wav");
-var RoboWestern = new Audio("www/asset/sounds/Robo-Western.wav");
-var OutlawLand = new Audio("www/asset/sounds/584409_Outlaw-Land.wav");
-var laser = new Audio("www/asset/sounds/Gun_Shot-Marvin-1140816320.wav");
+var Hustle = new Audio("assets/sounds/Hustle.wav");
+var RoboWestern = new Audio("assets/sounds/Robo-Western.wav");
+var OutlawLand = new Audio("assets/sounds/584409_Outlaw-Land.wav");
+var laser = new Audio("assets/sounds/Gun_Shot-Marvin-1140816320.wav");
 
 var bgArray = [Hustle, RoboWestern, OutlawLand];
 var sfxArray = [laser];
