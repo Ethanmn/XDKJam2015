2918aca9ad3a25fa59e4b8a4450ae8a11f163842
Ethan Nakashima <ethanmn@hotmail.com>
2015-02-28 09:24:34 -0800

    added bullets

---
+++ bullet.js
@@ -0,0 +1,29 @@
+var BULLET_VEL = 10;
+var BULLET_SIZE = 10;
+
+var Bullet = function(px, py, vx, vy)
+{
+   this.init = function(px, py, vx, vy)
+   {
+      this.posX = px;
+      this.posY = py;
+      this.velX = vx;
+      this.velY = vy;
+   }
+   this.init(px, py, vx, vy);
+   
+   this.update = function(delta)
+   {
+      
+      this.posX += this.velX * magic;
+      this.posY += this.velY * magic;
+      
+      //console.log("px " + this.posX + " py " + this.posY);
+      // Add collisions here
+   }
+   
+   this.draw = function(ctx)
+   {
+      ctx.drawImage(bulletImage, this.posX, this.posY);
+   }
+}
