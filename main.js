255ea65ea37d36bd5855c59137856db8101bb5f5
Simon Vurens <svurens@gmail.com>
2015-02-27 21:26:28 -0800

    basic stuff2

---
+++ main.js
@@ -0,0 +1,26 @@
+var delta = 20; // 50 FPS
+var ctx;
+function loadGame()
+{
+	ctx = document.getElementById("canvas").getContext('2d');
+   gameLoop();
+}
+
+function gameLoop()
+{
+	update(delta);
+	draw();
+
+	window.setTimeout(gameLoop, delta);
+}
+
+function update(delta) {
+   //do stuff here, happens every frame.
+}
+
+function draw()
+{
+	ctx.fillStyle = '#ff00ff';
+	ctx.fillRect(0,0, 500, 600);
+   ctx.stroke();
+}
