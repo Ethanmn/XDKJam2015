0c878f42305e52afafd0c738582f134d4686e6c6
Simon Vurens <svurens@gmail.com>
2015-02-28 18:48:36 -0800

    undo stuff

--- bullet.js
+++ bullet.js
@@ -1,6 +1,6 @@
 var BULLET_VEL = 10;
 var BULLET_SIZE = 6;
-var BULLET_COST = 10
+var BULLET_COST = 10;
 
 var Bullet = function(px, py, vx, vy)
 {
@@ -12,17 +12,17 @@ var Bullet = function(px, py, vx, vy)
       this.posY = py;
       this.velX = vx;
       this.velY = vy;
-   }
+   };
    this.init(px, py, vx, vy);
    
    this.update = function(delta)
    {
       this.posX += this.velX * magic;
       this.posY += this.velY * magic;
-   }
+   };
    
    this.draw = function(ctx)
    {
       ctx.drawImage(bulletImage, this.posX, this.posY);
-   }
-}
+   };
+};
